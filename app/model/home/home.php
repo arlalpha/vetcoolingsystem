7e33ffc062de7a344755b5790bad0d4bbb7aca72
arlalpha <78222579+arlalpha@users.noreply.github.com>
2021-01-30 09:08:53 +0800

    Update home.php

--- app/model/home/home.php
+++ app/model/home/home.php
@@ -1,6 +1,17 @@
 <?php 
 
-$name = "ALFEH RAE LAZARRA";
+$connection = new mysqli("localhost","SMGjE414dN","fgW0PWZeE3","SMGjE414dN");
+
+if(mysqli_connect_errno()){
+ echo "error"; 
+}
+
+echo "connected";
+
+
+
+
+mysqli_close($connection);
 
 
 ?>
