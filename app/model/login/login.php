e85b38dff709aac55423a4f226826dbe651ae431
arlalpha <78222579+arlalpha@users.noreply.github.com>
2021-01-30 09:58:03 +0800

    Update login.php

--- app/model/login/login.php
+++ app/model/login/login.php
@@ -1,17 +1,4 @@
 <?php
 
-$connection = mysqli_connect("localhost","SMGjE414dN","fgW0PWZeE3","SMGjE414dN");
-
-$query = "SELECT * FROM tblaccount WHERE Id=1";
-
-$result = mysqli_query($connection,$query);
-$getrow = mysql_num_rows($result);
-
-echo $getrow;
-
-
-
-mysqli_close($connection);
-
 
 ?>
