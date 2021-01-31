7dcab9e6c5267064a4347ec3974a3c9757e2763a
arlalpha <78222579+arlalpha@users.noreply.github.com>
2021-01-31 23:50:58 +0800

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -4,8 +4,8 @@ $config = [
     "MODEL_PATH" => APPLICATION_PATH . DS . "model" . DS,
     "VIEW_PATH" => APPLICATION_PATH . DS . "view" . DS,
     "LIB_PATH" => APPLICATION_PATH . DS . "lib" . DS,
-    "IMG_PATH" => "/vetcoolingsystem/app" . DS . "img" . DS,
-    "LIBZ_PATH" => "/vetcoolingsystem/app" . DS . "lib" . DS,
+    "IMG_PATH" => "/app" . DS . "img" . DS,
+    "LIBZ_PATH" => "/app" . DS . "lib" . DS,
 
 ];
 
